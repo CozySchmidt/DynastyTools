17fb9095bef95ade2d479d3cbc842388b09d4f0b
Dom-Marra <dominicmarra321@gmail.com>
2021-03-28 13:37:59 -0700

    Added Url for GetNextMatchUp

--- rankingsApp/urls.py
+++ rankingsApp/urls.py
@@ -1,9 +1,11 @@
-from django.contrib.auth import views
-from django.urls import path
+# from django.contrib.auth import views
+from django.urls import path, re_path
 from .views import *
+from rankingsApp import views
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('admin/', AdminView.as_view(), name='admin')
+    path('admin/', AdminView.as_view(), name='admin'),
+    re_path(r'^api/nextmatchup/$', views.GetNextMatchup)
 ]
 
