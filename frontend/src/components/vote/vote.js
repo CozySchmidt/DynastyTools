37a0837de20055e174dbc2a4d38df40614b7101e
Dom-Marra <dominicmarra321@gmail.com>
2021-03-28 20:44:15 -0700

    Redesigned code

--- frontend/src/components/vote/vote.js
+++ frontend/src/components/vote/vote.js
@@ -1,39 +1,92 @@
 import './vote.scss';
 import React, { Component } from "react";
 import { Typography } from '@material-ui/core';
-import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios';
-import { NEXT_MATCHUP } from '../../constants/api-urls';
+import axios from 'axios';
+import { NEXT_MATCHUP, INSERT_MATCHUP } from '../../constants/api-urls';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import VoteButton from '../votebutton/votebutton'
 
 class Vote extends Component {
-    
-    render() {
-        return (
-            <div id="vote-container">
-                <Post url={NEXT_MATCHUP} params={{position: "QB"}}>
-                    {(error, response, isLoading, makeRequest, axios) => {
-                        if(error) {
-                            return (
-                                <Typography variant="h5">
-                                    Error: {error.message}
-                                </Typography>
-                            );
-                        } else if (response !== null) {
-                            return (
-                                <Typography variant="h5">
-                                    {response.data.PlayerOneName} VS {response.data.PlayerTwoName} 
-                                </Typography>
-                            );
-                        } else if (isLoading) {
-                            return <p>Loading</p>
-                        }
 
-                        return <p>Default Message</p>
-                    }}
-                </Post>
-                
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            response: null
+        }
+    }
+
+    componentDidMount() {
+        this.nextMatchUp();
+    }
+
+    nextMatchUp = () => {
+        axios.post(NEXT_MATCHUP, {position: 'QB'}).then(res => {
+            this.setState({response: res})
+        }).catch(err => {
+            this.setState({error: err})
+        })
+    }
+
+    submitVote(winner) {
+        let data = this.state.response.data;
+        data.Winner = winner
+        console.log(data);
+        
+        axios.post(INSERT_MATCHUP, data).then(res => {
+            this.nextMatchUp();
+        }).catch(err => {
+            console.log(err.message)
+        })
+    }
+
+    /**
+     * Returns the inner html for the vote container depending on the current state
+     * 
+     * @returns 
+     */
+    getInnerContents() {
+        if (this.state.error) {
+            return (
+                <Typography variant="h5">
+                    Error: {this.state.error.message}
+                </Typography>
+            );
+        } else if (this.state.response !== null) {
+            return (
+                <div id="matchup-container">
+                    <div id="vote-button-container">
+                        <VoteButton 
+                            className="vote-button" 
+                            player={this.state.response.data.PlayerOne}
+                            onClick={() => this.submitVote(this.state.response.data.PlayerOne)}
+                        >
+                        </VoteButton>
+                        <VoteButton 
+                            className="vote-button" 
+                            player={this.state.response.data.PlayerTwo}
+                            onClick={() => this.submitVote(this.state.response.data.PlayerTwo)}
+                        >
+                        </VoteButton>
+                    </div>
+                </div>
+            );
+        }
+
+        return LOADING
+    }
+
+    render() {
+        let view = (
+            <div id="vote-container">
+                {this.getInnerContents()}
             </div>
         );
+
+        return view;
     }
 }
+
+const LOADING = <div id="matchup-loader"><CircularProgress className="loading-bar"/></div>
 
 export default Vote;
