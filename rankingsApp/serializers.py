bca03f1d64c1d473dc4a128bbd9b487d38c9fd2c
Dom-Marra <dominicmarra321@gmail.com>
2021-04-01 21:15:58 -0700

    Fixed bug issue where PlayerModels were being created instead of retrieved fro the matchup model

--- rankingsApp/serializers.py
+++ rankingsApp/serializers.py
@@ -6,10 +6,16 @@ class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlayerModel
         fields = ('id', 'Name', 'Rating', 'Team', 'Position')
+        extra_kwargs = {
+            "id": {
+                "read_only": False,
+                "required": False,
+            },
+        }
 
 
 class MatchupSerializer(serializers.ModelSerializer):
-    PlayerOne = PlayerSerializer(many=False)
+    PlayerOne = PlayerSerializer(many=False, )
     PlayerTwo = PlayerSerializer(many=False)
     Winner = PlayerSerializer(many=False, required=False)
     class Meta:
@@ -17,8 +23,8 @@ class MatchupSerializer(serializers.ModelSerializer):
         fields = ('PlayerOne', 'PlayerTwo', 'Winner')
 
     def create(self, validated_data):
-        PlayerOne = PlayerModel.objects.create(**validated_data.pop('PlayerOne'))
-        PlayerTwo = PlayerModel.objects.create(**validated_data.pop('PlayerTwo'))
-        Winner = PlayerModel.objects.create(**validated_data.pop('Winner'))
+        PlayerOne = PlayerModel.objects.get(id=validated_data.pop('PlayerOne')['id'])
+        PlayerTwo = PlayerModel.objects.get(id=validated_data.pop('PlayerTwo')['id'])
+        Winner = PlayerModel.objects.get(id=validated_data.pop('Winner')['id'])
         MatchUp = MatchupModel.objects.create(PlayerOne=PlayerOne, PlayerTwo=PlayerTwo, Winner=Winner)
         return MatchUp
