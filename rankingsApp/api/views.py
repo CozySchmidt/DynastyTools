7e0d9be375276e9d8288a717447297cf0407db38
Lewis Crabtree <lewis.crabtree98@gmail.com>
2021-04-02 17:41:02 -0700

    rest api filtering

--- rankingsApp/api/views.py
+++ rankingsApp/api/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
 from rankingsApp.api.serializers import *
 from rankingsApp.models import *
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PlayerListView(generics.ListAPIView):
     queryset = PlayerModel.objects.all().order_by('Position', '-Rating', 'Name')
     serializer_class = PlayerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['Name', 'Rating', 'Team', 'Position']
 
 class MatchupListView(generics.ListAPIView):
     queryset = MatchupModel.objects.all()
