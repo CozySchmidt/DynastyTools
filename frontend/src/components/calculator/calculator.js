478f8ae1a059a8b1f628e9a44ad34eab6ad08f33
Schmidt <theletroll@gmail.com>
2021-07-25 01:36:32 -0700

    Changed it so it returns empty div.

--- frontend/src/components/calculator/calculator.js
+++ frontend/src/components/calculator/calculator.js
@@ -10,53 +10,13 @@ import Tabs from "../tabs/tabs";
 
 class Calculator extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 0
-        }
-    }
-
     render() {
 
         return (
-            <div id="mat-tabs">
-                <AppBar position="static" elevation={0}>
-                    <Toolbar id="tabs-toolbar">
-                        <Tabs 
-                            value={this.state.value} 
-                            onChange={this.handleChange} 
-                            aria-label="page tabs"
-                            id="tabs"
-                        >
-                            <Tab label="Vote" />
-                            <Tab label="Rankings" />
-                            <Tab label="Calculator" />
-                        </Tabs>
-                        <Typography id="tabs-heading" variant="h5">
-                            Consensus Rankings
-                        </Typography>
-                    </Toolbar>
-                
-                </AppBar>
-                <div role="tabpanel" className="tab-panel" hidden={this.state.value !== 0}>
-                    <Vote></Vote>
-                </div>
-                
-                <div role="tabpanel" className="tab-panel"  hidden={this.state.value !== 1}>
-                    <Rankings></Rankings>
-                </div> 
-
-                <div role="tabpanel" className="tab-panel"  hidden={this.state.value !== 1}>
-                    <Calculator></Calculator>
-                </div> 
+            <div>
             </div>
         );
     }
-
-    handleChange = (event, newValue) => {
-        this.setState({value: newValue})
-    }
 }
 
-export default Nav;
+export default Calculator;
