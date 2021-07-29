c97e94380806a63073ec15a86c0322688df71bdb
LewisCrabtree <lewis.crabtree98@gmail.com>
2021-07-28 22:07:40 -0700

    adjust heroku settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { MuiThemeProvider, unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core/styles';  
 import Nav from './components/tabs/tabs'
 import './App.css';
+import axios from 'axios';
+
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 const theme = createMuiTheme({
   palette: {
