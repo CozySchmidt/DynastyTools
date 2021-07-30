628afd9c85015e44565250e27b303c33f4e6f597
LewisCrabtree <lewis.crabtree98@gmail.com>
2021-07-29 20:27:52 -0700

    add staticfiles_dirs

--- rankingsProject/settings.py
+++ rankingsProject/settings.py
@@ -164,6 +164,7 @@ REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
